9462f906001c8c4f151110c3f99c7a268f4a67b0
Mahadiputra <6297984+mahadi22@users.noreply.github.com>
2020-10-14 14:23:57 +0700

    Update InstaDownloader.user.js

--- InstaDownloader.user.js
+++ InstaDownloader.user.js
@@ -13,23 +13,6 @@
 // @grant           none
 // ==/UserScript==
 
-/*
- * # Instructions.
- *
- * Simply hold down a modifier key and click on any Instagram photo, video or profile avatar!
- *
- * - `Shift-click`: View in the same tab.
- * - `Alt-click`: View in a new tab/window.
- * - `Shift-Alt-click`: Direct download if supported by browser.
- *
- * Alternatively, you can use the keyboard controls, which are definitely a lot more convenient
- * if you're already using Instagram's own `left`/`right`-arrow navigation to switch between media!
- *
- * - `Shift-F`: View in the same tab.
- * - `Alt-F`: View in a new tab/window.
- * - `Shift-Alt-F`: Direct download if supported by browser.
- */
-
 (function() {
     'use strict';
 
